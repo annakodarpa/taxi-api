6b9f33223997adac2ec677919b76575fee4ceaff
Anna Wikström <anna@digitaledge.se>
2024-05-29 13:55:29 +0200

    FIX _id not visible in /rides

--- src/models/RideRequest.ts
+++ src/models/RideRequest.ts
@@ -2,6 +2,7 @@ import mongoose, { Schema, Document } from 'mongoose';
 let mongooseHidden = require('mongoose-hidden')()
 
 interface IRideRequest extends Document {
+    _id: { type: mongoose.Types.ObjectId, auto: true },
     clientId: string;
     // clientId: mongoose.Types.ObjectId; // should be an objectID, but doing string for this assignment
     pickupLocation: string;
@@ -16,6 +17,7 @@ interface IRideRequest extends Document {
 }
 
 const RideRequestSchema: Schema = new Schema({
+    _id: { type: mongoose.Types.ObjectId, auto: true, hide: false },
     clientId: { type: String, required: true, ref: 'Client' },
     pickupLocation: { type: String, required: true },
     dropoffLocation: { type: String, required: true },
